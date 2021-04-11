26d512569ea6b93b4539b3fdb1d2302f933e93a1
ausi1997.com <ausafen1018@gmail.com>
2021-04-11 11:42:24 +0530

    commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();           // importing the required modules
+const bodyParser = require('body-parser');
+//const cors = require('cors');
+require('dotenv').config();
+
+// importing the  db module
+const db = require('./models/db');
+
+const userRoutes = require('./routes/user');
+
+const postRoutes = require('./routes/post');
+
+//app.use(cors());
+
+ // parse requests of content-type - application/json
+ app.use(bodyParser.json());
+
+ // parse requests of content-type - application/x-www-form-urlencoded
+ app.use(bodyParser.urlencoded({ extended: true }));
+ 
+
+// default route
+app.get('/',(req,res)=>{
+    res.send('Wlecome to instagram');
+});
+
+// user route
+app.use('/user', userRoutes);
+
+// post route
+app.use('/post', postRoutes);
+
+/*if(process.env.NODE_ENV == "production"){
+    app.use(express.static('client/build'))
+    const path =require('path');
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}*/
+
+// assigning the port
+const PORT =process.env.PORT || 7001;
+app.listen(PORT,()=>{
+    console.log('Server is running on the port 7001');
+});
